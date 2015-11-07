b538280f08870ee1f35ba0992249ba6f99992dc1
Túlio Braga <tuliobragam@gmail.com>
2015-11-07 13:52:54 -0200

    mensagens login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,18 +41,91 @@ angular.module('starter.controllers', [])
 
 .controller('RankingDePratosCtrl', function($scope) {
 
+  $scope.top5 = [
+    {
+      title: 'DIA 11/05/15',
+      components: [
+        {title: "Arroz", note: ""},
+        {title: "Feijão", note: ""},
+        {title: "Bife acebolado", note: ""},
+        {title: "Almeirão com cenouras", note: ""},
+      ],
+      comments: [{},{}],
+      likes: []
+    },
+    {
+      title: 'DIA 11/05/15',
+      components: [
+        {title: "Arroz", note: ""},
+        {title: "Feijão", note: ""},
+        {title: "Bife acebolado", note: ""},
+        {title: "Almeirão com cenouras", note: ""},
+      ]
+    },
+    {
+      title: 'DIA 11/05/15',
+      components: [
+        {title: "Arroz", note: ""},
+        {title: "Feijão", note: ""},
+        {title: "Bife acebolado", note: ""},
+        {title: "Almeirão com cenouras", note: ""},
+      ]
+    },
+    {
+      title: 'DIA 11/05/15',
+      components: [
+        {title: "Arroz", note: ""},
+        {title: "Feijão", note: ""},
+        {title: "Bife acebolado", note: ""},
+        {title: "Almeirão com cenouras", note: ""},
+      ]
+    },
+    {
+      title: 'DIA 11/05/15',
+      components: [
+        {title: "Arroz", note: ""},
+        {title: "Feijão", note: ""},
+        {title: "Bife acebolado", note: ""},
+        {title: "Almeirão com cenouras", note: ""},
+      ]
+    }
+  ];
+
 })
 
 .controller('AlertasCtrl', function($scope) {
 
 })
 
-.controller('LoginCtrl', function($scope, $state) {
+.controller('LoginCtrl', function($scope, $state, $ionicPopup) {
   
   $scope.loginData = {};
 
   $scope.doLogin = function() {
-    $state.go('app.pratoDoDia');
+    if($scope.loginData.username == "aluno@cefetmg.br" && $scope.loginData.password == "123") {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Sucesso',
+        template: 'Login efetuado com sucesso!'
+      });
+      alertPopup.then(function(res) {
+        $state.go('app.pratoDoDia');
+      });
+    } else if ($scope.loginData.username == "aluno@cefetmg.br" && $scope.loginData.password && $scope.loginData.password  != "123") {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Falha',
+        template: 'Senha não corresponde ao email digitado!'
+      });
+    } else if ($scope.loginData.username && $scope.loginData.password) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Falha',
+        template: 'Nenhum usuário encontrado para este email!'
+      });
+    } else {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Falha',
+        template: 'Email e senha são obrigatórios para efetuar login.'
+      });
+    }
   };
 
   $scope.signUp = function() {
