97d88f09dd90967737f336f0889fcfe0b2f3de8d
Túlio Braga <tuliobragam@gmail.com>
2015-11-07 16:20:12 -0200

    sign up client validations

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,17 @@ var showError = function(text, $ionicPopup) {
   return alertPopup;
 }
 
+var enter = function (text, $state, $ionicPopup) {
+  var alertPopup = $ionicPopup.alert({
+    title: 'Sucesso',
+    template: text
+  });
+  alertPopup.then(function(res) {
+    $state.go('app.pratoDoDia');
+  });
+  return alertPopup;
+};
+
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
@@ -125,13 +136,14 @@ angular.module('starter.controllers', [])
 
         // simulate server-side login
         try {
-          if(login())
-            enter();
+          if(login()) {
+            var text = "Login efetuado com sucesso!";
+            enter(text, $state, $ionicPopup);
+          }
         } catch(e) {
           showError(e, $ionicPopup);
         }
       }, 2000);
-
     }
   };
 
@@ -144,16 +156,6 @@ angular.module('starter.controllers', [])
     }
 
     throw "Nenhum usuário encontrado para este email!"
-  };
-
-  var enter = function () {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Sucesso',
-      template: 'Login efetuado com sucesso!'
-    });
-    alertPopup.then(function(res) {
-      $state.go('app.pratoDoDia');
-    });
   };
 
   $scope.signUp = function() {
@@ -180,17 +182,51 @@ angular.module('starter.controllers', [])
   
 })
 
-.controller('SignUpCtrl', function($scope, $state) {
+.controller('SignUpCtrl', function($scope, $state, $timeout, $ionicPopup, $ionicLoading) {
   
-  $scope.singUpData = {};
+  $scope.signUpData = {};
 
-  $scope.signUp = function() {
-    if($scope.singUpData.firstName && $scope.singUpData.lastName && 
-       $scope.singUpData.gender && $scope.singUpData.email &&
-       $scope.singUpData.password && $scope.singUpData.confirmPassword) {
+  $scope.doSignUp = function() {
+        console.log($scope.signUpData.firstName);
+    console.log($scope.signUpData.lastName);
+    console.log($scope.signUpData.email);
+    console.log($scope.signUpData.password);
+    console.log($scope.signUpData.confirmPassword);
+    console.log($scope.signUpData.gender);
+    console.log($scope.signUpData.termsAccepted);
+    if(!$scope.signUpData.firstName || !$scope.signUpData.lastName || 
+       !$scope.signUpData.gender || !$scope.signUpData.email ||
+       !$scope.signUpData.password || !$scope.signUpData.confirmPassword) {
+      showError('Todos os campos são obrigatórios para efetuar cadastro.', $ionicPopup);
+    } else if ($scope.signUpData.termsAccepted != true){
+      showError('É necessário aceitar os termos de uso para proceder com o cadastro.', $ionicPopup);
+    } else if ($scope.signUpData.password != $scope.signUpData.confirmPassword){
+      showError('Senha e confirmação de senha precisam ser iguais.', $ionicPopup);
+    } else {
+      // Start sign up
+      $ionicLoading.show({
+        template: 'Efetuando cadastro...'
+      });
 
+      // simulate user wait
+      $timeout(function() {
+        $ionicLoading.hide();
+
+        // simulate server-side login
+        try {
+          if(signUp())
+            var msg = "Cadastro efetuado com sucesso! Um e-mail de confirmação foi enviado para você, favor verificar sua caixa de entrada. Caso não o encontre, certifique-se de que o mesmo não tenha sido filtrado para a caixa de spams.";
+            enter(msg, $state, $ionicPopup);
+        } catch(e) {
+          showError(e, $ionicPopup);
+        }
+      }, 2000);
     }
   };
+
+  var signUp = function() {
+    return true;
+  }
 
   $scope.termsOfUsage = function() {
     $state.go('termosDeUso');
